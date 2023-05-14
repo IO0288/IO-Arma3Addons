4c1feef298f5782dc430354a4efe311e7a313d28
IO0288 <admin@io0288.cn>
2023-05-15 04:30:20 +0800

    feat:acex-sps-337

---
+++ addons/aceax-sps-337/XtdGearInfos.hpp
@@ -0,0 +1,5 @@
+class XtdGearInfos {
+    class CfgWeapons {
+         #include "XtdGearInfos\config\CfgWeapons\SPS_hk337.hpp"
+    };
+};
---
+++ addons/aceax-sps-337/XtdGearInfos/config/CfgWeapons/SPS_hk337.hpp
@@ -0,0 +1,66 @@
+class SPS_hk337_vfg_black_f {
+  model = "SPS_hk337";
+  addonsgroup = "black_f";
+  afg = "vfg_f";
+  barrelgroup = "black_f";
+  boltgroup = "black_f";
+  receiver = "black_f";
+  ris = "black_f";
+  stock = "black_f";
+  vertgrip = "vfg_black_f";
+};
+class SPS_hk337_vfg_FDE_f {
+  model = "SPS_hk337";
+  addonsgroup = "f";
+  afg = "vfg_f";
+  barrelgroup = "f";
+  boltgroup = "f";
+  receiver = "FDE_f";
+  ris = "FDE_f";
+  stock = "f";
+  vertgrip = "vfg_f";
+};
+class SPS_hk337_vfg_RAL8000_f {
+  model = "SPS_hk337";
+  addonsgroup = "f";
+  afg = "vfg_f";
+  barrelgroup = "f";
+  boltgroup = "f";
+  receiver = "RAL8000_f";
+  ris = "RAL8000_f";
+  stock = "f";
+  vertgrip = "vfg_f";
+};
+class SPS_hk337_afg_black_f {
+  model = "SPS_hk337";
+  addonsgroup = "black_f";
+  afg = "afg_black_f";
+  barrelgroup = "black_f";
+  boltgroup = "black_f";
+  receiver = "black_f";
+  ris = "black_f";
+  stock = "black_f";
+  vertgrip = "afg_f";
+};
+class SPS_hk337_afg_FDE_f {
+  model = "SPS_hk337";
+  addonsgroup = "f";
+  afg = "afg_f";
+  barrelgroup = "f";
+  boltgroup = "f";
+  receiver = "FDE_f";
+  ris = "FDE_f";
+  stock = "f";
+  vertgrip = "afg_f";
+};
+class SPS_hk337_afg_RAL8000_f {
+  model = "SPS_hk337";
+  addonsgroup = "f";
+  afg = "afg_f";
+  barrelgroup = "f";
+  boltgroup = "f";
+  receiver = "RAL8000_f";
+  ris = "RAL8000_f";
+  stock = "f";
+  vertgrip = "afg_f";
+};
