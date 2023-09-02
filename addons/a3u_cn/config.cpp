08c8c73c3757ad231708037f9211afd75e581489
IO0288 <admin@io0288.cn>
2023-09-02 23:50:38 +0800

    feat:add A3U zh_cn

---
+++ addons/a3u_cn/config.cpp
@@ -0,0 +1,20 @@
+#include "script_version.hpp"
+class CfgPatches
+{
+	class a3u_cn
+	{
+		name = "a3u_cn - IO0288";
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 2.10;// 所需Arma3版本
+		requiredAddons[] =
+			{
+				"A3U_Mod"
+			};
+		author = "IO0288";
+		authors[] =
+			{"IO0288"};
+		url = "https://git.io0288.cn/IO0288/IO-Arma3Addons";
+		versionAr[] = {MAJOR, MINOR, PATCH, BUILD};
+	};
+};
