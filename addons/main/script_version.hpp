f35d6703c86fc3427b4c2e162cf0f5787e4cd213
IO0288 <admin@io0288.cn>
2024-06-24 16:18:58 +0800

    docs(ALL): 使用日期作为版本

--- addons/main/script_version.hpp
+++ addons/main/script_version.hpp
@@ -3,7 +3,7 @@
 #undef PATCH
 #undef BUILD
 
-#define MAJOR 1
-#define MINOR 1
-#define PATCH 0 // `#define PATCHLVL` can also be used
-#define BUILD 1
+#define MAJOR 24
+#define MINOR 06
+#define PATCH 24 // `#define PATCHLVL` can also be used
+#define BUILD 01
