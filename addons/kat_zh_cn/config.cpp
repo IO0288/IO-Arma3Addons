ff2e663b30cf8d03ea7c92fe405ecfad7d0b9ac5
IO0288 <admin@io0288.cn>
2024-07-01 20:26:16 +0800

    feat(kat-zh_cn): 加入KAT汉化包

---
+++ addons/kat_zh_cn/config.cpp
@@ -0,0 +1,24 @@
+class CfgPatches
+{
+	class kat_zh_cn
+	{
+		name="KAT - KAT GUI";
+		requiredVersion=2.1400001;
+		units[]={};
+		weapons[]={};
+		magazines[]={};
+		requiredAddons[]=
+		{
+			"kat_gui"
+		};
+		author="IO0288";
+		authors[]=
+		{
+			"IO0288"
+		};
+		url="$STR_kat_main_URL";
+		version=2.1400001;
+		versionStr="2.14.2";
+		versionAr[]={2,14,2};
+	};
+};
