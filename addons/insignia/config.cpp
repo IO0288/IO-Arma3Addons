50a3f8850e9afc2a1ca6d5f929f52c0131d5172d
IO0288 <admin@io0288.cn>
2024-08-23 18:00:46 +0800

    feat(insignia):add odst houq

---
+++ addons/insignia/config.cpp
@@ -0,0 +1,25 @@
+// Images code
+
+class CfgUnitInsignia {
+	//  ODST  //
+	class odst_houq{
+		displayName = "[ODST] 后勤";
+		texture = "\ioaa3_def_insignia\data\odst\houq.paa";
+		author = "IO0288";
+	};
+};
+
+class UniformSlotInfo {
+	slotType = 0;
+	linkProxy = "-";
+};
+
+class CfgPatches {
+	class IOAA3_Insignia {
+        author = "IO0288";
+		units[] = {};
+		weapons[] = {};
+		requiredVersion = 1.0;
+		requiredAddons[] = {"A3_Weapons_F"};
+	};
+};
